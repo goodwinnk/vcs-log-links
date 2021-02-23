6c9bb46b56172c6a24abd02dc256bdd4da64cd44
Nikolay Krasko <goodwinnk@gmail.com>
2021-02-23 03:24:35 +0300

    Column with links in the commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.jetbrains.intellij") version "0.6.5"
+    kotlin("jvm") version "1.4.21"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "203.7148.57"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
+
+allprojects {
+    //Support @JvmDefault
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjvm-default=enable")
+            jvmTarget = "1.8"
+        }
+    }
+}
---
+++ src/main/kotlin/com/nkrasko/extracts/idea/plugin/IssuesCustomColumn.kt
@@ -0,0 +1,58 @@
+package com.nkrasko.extracts.idea.plugin
+
+import com.intellij.openapi.util.TextRange
+import com.intellij.openapi.vcs.IssueNavigationConfiguration
+import com.intellij.openapi.vcs.IssueNavigationConfiguration.LinkMatch
+import com.intellij.openapi.vcs.VcsBundle
+import com.intellij.vcs.log.graph.DefaultColorGenerator
+import com.intellij.vcs.log.paint.GraphCellPainter
+import com.intellij.vcs.log.paint.SimpleGraphCellPainter
+import com.intellij.vcs.log.ui.render.GraphCommitCell
+import com.intellij.vcs.log.ui.render.GraphCommitCellRenderer
+import com.intellij.vcs.log.ui.table.GraphTableModel
+import com.intellij.vcs.log.ui.table.VcsLogGraphTable
+import com.intellij.vcs.log.ui.table.column.VcsLogCustomColumn
+import javax.swing.table.TableCellRenderer
+
+@Suppress("UnstableApiUsage")
+class IssuesCustomColumn : VcsLogCustomColumn<GraphCommitCell> {
+    override val id: String = "com.nkrasko.extracts.idea.plugin.ExtractsCustomColumn"
+    override val isDynamic: Boolean = true
+    override val localizedName: String = VcsBundle.message("issue.link.link.column")
+
+    override fun createTableCellRenderer(table: VcsLogGraphTable): TableCellRenderer {
+        val graphCellPainter: GraphCellPainter = object : SimpleGraphCellPainter(DefaultColorGenerator()) {
+            override fun getRowHeight(): Int = table.rowHeight
+        }
+
+        return GraphCommitCellRenderer(table.logData, graphCellPainter, table)
+    }
+
+    override fun getStubValue(model: GraphTableModel): GraphCommitCell = GraphCommitCell("", emptyList(), emptyList())
+
+    override fun getValue(model: GraphTableModel, row: Int): GraphCommitCell {
+        val issueNavigationConfiguration = IssueNavigationConfiguration.getInstance(model.logData.project)
+        val fullMessage = model.getCommitMetadata(row).fullMessage
+        val issueLinks: List<LinkMatch> = issueNavigationConfiguration.findIssueLinks(fullMessage)
+        val text = linksToText(fullMessage, issueLinks)
+
+        return GraphCommitCell(
+            text,
+            emptyList(),
+            emptyList()
+        )
+    }
+
+    companion object {
+        private fun TextRange.toKotlinRange(): IntRange = startOffset until endOffset
+
+        fun linksToText(message: String, links: List<LinkMatch>): String {
+            return links
+                .map { link ->
+                    message.substring(link.range.toKotlinRange())
+                }
+                .toSet()
+                .joinToString(" ")
+        }
+    }
+}
